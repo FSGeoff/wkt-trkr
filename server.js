c6ceaddefee87145ab246d5619e8a0f4f4f703ef
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-03 21:50:19 -0500

    added models and some routes, still needs work

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
@@ -20,6 +21,44 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useUnifiedTopology: true,
 	useFindAndModify: false,
 	useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+	console.log("Mongoose connected successfully!");
+});
+
+connection.on("error", (err) => {
+	console.log("Mongoose connection error: " + err);
+});
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/api/config", (req, res) => {
+	res.json({
+		success: true,
+	});
+});
+
+app.get("api/workout", (req, res) => {
+	db.Workout.find().then((foundWorkout) => {
+		res.json(foundWorkout);
+	});
+});
+
+app.get("api/workout/:id", (req, res) => {
+	db.Workout.findById(req.params.id).then((foundWorkout) => {
+		res.json(foundWorkout);
+	});
+});
+
+app.post("api/workout", (req, res) => {
+	db.Workout.create(req.body).then((newWorkout) => {
+		res.json(newWorkout);
+	});
 });
 
 app.listen(PORT, () => {
